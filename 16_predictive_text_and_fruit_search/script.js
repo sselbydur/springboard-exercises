be2dd31dd756bc3dac6bfb6fec0e9d9deff4388f
Rudy Bless <rudys.bless@gmail.com>
2023-06-21 13:35:58 -0500

    updating files

--- 16_predictive_text_and_fruit_search/script.js
+++ 16_predictive_text_and_fruit_search/script.js
@@ -4,17 +4,18 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 
-
 function search(str) {
 	let results = [];
 
 	// TODO
-
-	for(let i = 0; i < fruit.length; i++){
-		if([i].some((str) => [i].includes(str))){
-			results.push([i])
-		}
+	for (let i = 0; i < fruit.length; i++) {
+		fruit[i].some(function(partialString){
+			if(true){
+				results.push(fruit[i])
+			}
+		})
 	}
+
 	return results;
 	console.log(results)
 }
@@ -35,6 +36,10 @@ function useSuggestion(e) {
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
+
+function double (num){
+	return num * 2
+}
 
 
 		// const individualFruits = Array.from(fruit[i].toLowerCase());
@@ -61,3 +66,24 @@ suggestions.addEventListener('click', useSuggestion);
 
 		// console.log(individualFruits)
 		// console.log(userInput)
+
+			// for(let i = 0; i < fruit.length; i++){
+	// 	if(fruit.some((str) => fruit.includes(str))){
+	// 		results.push(i.value)
+	// 	}
+	// }
+
+		// for (let i = 0; i < fruit.length; i++){
+	// 	if (fruit.some(function(str){
+	// 		return str.indexOf(str) !== -1
+	// 	}){
+	// 		results.push(i);
+	// 	})
+	// } 
+
+		// for(let i = 0; i < fruit.length; i++){
+	// 	fruit[i].some(function(partialString){
+	// 		if (true){
+	// 			results.push(fruit[i])
+	// 		})
+	// 	}
